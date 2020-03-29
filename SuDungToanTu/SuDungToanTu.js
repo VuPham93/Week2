5c200998523a7ef66c43327b366564cd9cc288fb
VuPham93 <62596412+VuPham93@users.noreply.github.com>
2020-03-29 13:13:59 +0700

    Add SuDungToanTu folder

---
+++ SuDungToanTu/SuDungToanTu.js
@@ -0,0 +1,10 @@
+function Toantu(){
+    let inputWidth;
+    let inputHeight;
+    inputWidth = prompt("Chiều dài:");
+    inputHeight = prompt("Chiều rộng");
+    let width = parseInt(inputWidth);
+    let height = parseInt(inputHeight);
+    let area = width * height;
+    document.write("Diện tích: " + area);
+}
